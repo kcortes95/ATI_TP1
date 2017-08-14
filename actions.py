bc1253ff335446e5e11688e1027d6abe105fb384
LucasCasa <lcasagrande@itba.edu.ar>
2017-08-14 20:58:38 -0300

    separacion de archivos

---
+++ actions.py
@@ -0,0 +1,54 @@
+from tkinter import Tk, Toplevel, Entry, Label, Button, messagebox, Menu, filedialog, Canvas
+from PIL import Image, ImageTk
+import numpy as np
+
+
+def set_color(self):
+    color = int(self.color_text.get())
+    self.canvas.true_image.putpixel((int(self.x_text.get()), int(self.y_text.get())), color)
+    self.canvas.image = ImageTk.PhotoImage(self.canvas.true_image)
+    self.canvas.create_image((0, 0), anchor="nw", image=self.canvas.image)
+
+
+def crop(self, master):
+    self.new_window = Toplevel()
+    self.new_window.minsize(width=640, height=480)
+    self.new_window.config(menu=self.menubar)
+    img = self.canvas.true_image.load()
+    ydif = abs(self.y_finish - self.y_start)
+    xdif = abs(self.x_finish - self.x_start)
+    new_image = np.zeros((ydif, xdif), dtype=np.uint8)
+    x_starter = 0 if self.x_start < self.x_finish else xdif - 1
+    x = x_starter
+    y = 0 if self.y_start < self.y_finish else ydif - 1
+    x_advance = 1 if self.x_start < self.x_finish else -1
+    y_advance = 1 if self.y_start < self.y_finish else -1
+    print("x from " + str(self.x_start) + " to  " + str(self.x_finish))
+    print("y from " + str(self.y_start) + " to  " + str(self.y_finish))
+    for x_pos in range(self.x_start, self.x_finish, x_advance):
+        for y_pos in range(self.y_start, self.y_finish, y_advance):
+            print(str(x_pos) + " " + str(y_pos))
+            aux = img[y_pos, x_pos]
+            new_image[y, x] = aux
+            x += x_advance
+        y += y_advance
+        x = x_starter
+    true_cropped = Image.fromarray(new_image, 'L')
+    cropped = ImageTk.PhotoImage(true_cropped)
+    self.new_window.canvas = Canvas(self.new_window, width=200, height=200)
+    self.new_window.canvas.true_cropped = true_cropped
+    self.new_window.canvas.cropped = cropped
+    self.new_window.canvas.configure(width=true_cropped.width, height=true_cropped.height)
+    self.new_window.canvas.create_image((0, 0), anchor="nw", image=cropped)
+    self.new_window.canvas.pack()
+
+
+def get_area_info(self, master):
+    pixel_count = abs(self.x_start - self.x_finish) * abs(self.y_start - self.y_finish)
+    print("Cantidad de pixeles:" + str(pixel_count))
+    img = self.canvas.true_image.load();
+    total = 0
+    for i in range(self.x_start, self.x_finish):
+        for j in range(self.y_start, self.y_finish):
+            total += img[i, j]
+    print("Promedio:" + str(total/pixel_count))
---
+++ mouse.py
@@ -0,0 +1,30 @@
+def select_pixel(event):
+    print(self.canvas.true_image.getpixel((event.x, event.y)))
+
+
+def set_pixel(event):
+    self.x_text.delete(0, len(self.x_text.get()))
+    self.y_text.delete(0, len(self.y_text.get()))
+    self.x_text.insert(0, event.x)
+    self.y_text.insert(0, event.y)
+
+
+def set_area(event):
+    print(str(event.x) + " " + str(event.y))
+    if self.release:
+        self.x_start = event.x
+    else:
+        self.x_finish = event.x
+
+    if self.release:
+        self.y_start = event.y
+        self.release = False;
+    else:
+        self.y_finish = event.y
+
+    self.canvas.coords(self.canvas.rect, self.x_start, self.y_start, self.x_finish, self.y_finish);
+
+
+def release_left(event,self):
+    self.release = True;
+    self.c
