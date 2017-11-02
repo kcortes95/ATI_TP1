eed4d024e61e91dbe00d8a932fa1ed54b59eed13
Kevin <kcortes.95@gmail.com>
2017-11-02 09:48:08 -0300

    vuelvo como antes

--- interface.py
+++ interface.py
@@ -114,8 +114,6 @@ class MyFirstGUI:
         filemenu = Menu(menubar, tearoff=0)
         filemenu.add_command(label="Canny", command=lambda: self.apply_method(canny.canny_function))
         filemenu.add_command(label="Susan All", command=lambda: self.apply_method(susan.susan_function))
-        filemenu.add_command(label="Susan Border", command=lambda: self.apply_method(susan.susan_function_borde))
-        filemenu.add_command(label="Susan Corner", command=lambda: self.apply_method(susan.susan_function_esquina))
         menubar.add_cascade(label="Detector", menu=filemenu)
 
         master.config(menu=menubar)
--- susan.py
+++ susan.py
@@ -14,28 +14,10 @@ def susan_function(matrix):
 
     circular_mask = [[0, 0, 1, 1, 1, 0, 0], [0, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 0], [0, 0, 1, 1, 1, 0, 0]]
 
-    return apply_mesh(img_arr, circular_mask, 7, 27, 'all')
-
-def susan_function_borde(matrix):
-    img_arr = matrix
-    width, height = matrix.shape
-    img_aux = np.zeros((width, height, 1), dtype=np.uint8)
-
-    circular_mask = [[0, 0, 1, 1, 1, 0, 0], [0, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 0], [0, 0, 1, 1, 1, 0, 0]]
-
-    return apply_mesh(img_arr, circular_mask, 7, 27, 'borde')
-
-def susan_function_esquina(matrix):
-    img_arr = matrix
-    width, height = matrix.shape
-    img_aux = np.zeros((width, height, 1), dtype=np.uint8)
-
-    circular_mask = [[0, 0, 1, 1, 1, 0, 0], [0, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 0], [0, 0, 1, 1, 1, 0, 0]]
-
-    return apply_mesh(img_arr, circular_mask, 7, 27, 'esquina')
+    return apply_mesh(img_arr, circular_mask, 7, 27, type)
 
 
-def apply_mesh(matrix, mesh, size, threshold, type):
+def apply_mesh(matrix, mesh, size, threshold):
     shape = matrix.shape
     print(shape)
     if len(shape) > 2:
@@ -44,9 +26,9 @@ def apply_mesh(matrix, mesh, size, threshold, type):
             out[:, :, i] = apply_mesh_one_dimension(matrix[:, :, i], mesh, size)
         return actions.linear_transform(out).astype(np.uint8)
     else:
-        return actions.linear_transform(apply_mesh_one_dimension(matrix, mesh, size, threshold, type)).astype(np.uint8)
+        return actions.linear_transform(apply_mesh_one_dimension(matrix, mesh, size, threshold)).astype(np.uint8)
 
-def apply_mesh_one_dimension(matrix, mesh, size, threshold, type):
+def apply_mesh_one_dimension(matrix, mesh, size, threshold):
 
     width, height = matrix.shape
     out = np.zeros((width, height, 3), dtype=np.float32)
@@ -58,14 +40,7 @@ def apply_mesh_one_dimension(matrix, mesh, size, threshold, type):
             if i >= shape[0] - radius or i < radius or j < radius or j >= shape[1] - radius:
                 out[i, j] = [0, 0, 0]
             else:
-                value = -1
-                if ( type == 'borde' ):
-                    value = calculate_borde(matrix, mesh, i, j, width, height, threshold)
-                elif (type == 'esquina'):
-                    value = calculate_esquina(matrix, mesh, i, j, width, height, threshold)
-                else:
-                    value = calculate(matrix, mesh, i, j, width, height, threshold)
-
+                value = calculate(matrix, mesh, i, j, width, height, threshold)
                 if (value == 0):
                     v = matrix[i][j]
                     out[i, j] = [v, v, v]
@@ -94,44 +69,6 @@ def calculate(matrix, mesh, i, j, width, height, threshold):
 
     if math.fabs(tot - 0.5) < 0.1:
         return 1
-
-    if( math.fabs(tot - 0.75) < 0.1 ):
-        return 2
-
-    return 0
-
-def calculate_borde(matrix, mesh, i, j, width, height, threshold):
-    sum = 0
-    central_pixel = int(matrix[i][j])
-
-    for k in range(-3, 4):
-        for l in range(-3, 4):
-            posx = i + k
-            posy = j + l
-            if mesh[k + 3][l + 3] != 0:
-                if math.fabs(matrix[posx][posy] - central_pixel) < threshold:
-                        sum += 1
-
-    tot = 1 - sum/37  #37 total en la máscara (CREO)
-
-    if math.fabs(tot - 0.5) < 0.1:
-        return 1
-
-    return 0
-
-def calculate_esquina(matrix, mesh, i, j, width, height, threshold):
-    sum = 0
-    central_pixel = int(matrix[i][j])
-
-    for k in range(-3, 4):
-        for l in range(-3, 4):
-            posx = i + k
-            posy = j + l
-            if mesh[k + 3][l + 3] != 0:
-                if math.fabs(matrix[posx][posy] - central_pixel) < threshold:
-                        sum += 1
-
-    tot = 1 - sum/37  #37 total en la máscara (CREO)
 
     if( math.fabs(tot - 0.75) < 0.1 ):
         return 2
